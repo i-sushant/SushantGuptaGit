a30260e3b73b2837d8e8a0b420cf61d916b403f0
Sushant <DELL@DESKTOP-EJJ66O4.(none)>
2020-02-04 20:13:02 +0530

    First commit

---
+++ src/main/java/com/epam/git/App.java
@@ -0,0 +1,14 @@
+package com.epam.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Greetings greetings = new Greetings();
+        greetings.greet();
+    }
+}
---
+++ src/main/java/com/epam/git/Greetings.java
@@ -0,0 +1,8 @@
+package com.epam.git;
+
+public class Greetings {
+	
+	public void greet() {
+		System.out.println("Hello World");
+	}
+}
